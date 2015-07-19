cafa02cd84843bebc9b8630c3957bff0b631e57d
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-07-19 10:05:25 +0200

    Create space-inspector_sv.ts
    Swedish translation if wanted.

---
+++ i18n/space-inspector_sv.ts
@@ -0,0 +1,249 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv" sourcelanguage="en_US">
+<context>
+    <name>ActivityIndicator</name>
+    <message>
+        <source>Estimating
+space usage.</source>
+        <translation>Utvärderar
+utrymmesanvändning.</translation>
+    </message>
+    <message>
+        <source>This might take
+some seconds.</source>
+        <translation>Det här kan ta
+några sekunder.</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>%1 
+ used of 
+ %2 
+ (%3)</source>
+        <translation>%1
+använt av
+%2
+(%3)</translation>
+    </message>
+</context>
+<context>
+    <name>Engine</name>
+    <message>
+        <source>Destination does not exist</source>
+        <translation>Målmappen finns inte</translation>
+    </message>
+    <message>
+        <source>Can&apos;t overwrite itself</source>
+        <translation>Kan inte skriva över sig själv</translation>
+    </message>
+    <message>
+        <source>Can&apos;t move/copy to itself</source>
+        <translation>Kan inte flytta/kopiera till sig själv</translation>
+    </message>
+    <message>
+        <source>File does not exist
+%1</source>
+        <translation>Filen finns inte
+%1</translation>
+    </message>
+    <message>
+        <source>Broken symbolic link
+%1</source>
+        <translation>Bruten symbolisk länk
+%1</translation>
+    </message>
+    <message>
+        <source>Can&apos;t read this type of file
+%1</source>
+        <translation>Kan inte läsa den här filtypen
+%1</translation>
+    </message>
+    <message>
+        <source>No permission to read the file
+%1</source>
+        <translation>Ej behörig att läsa filen
+%1</translation>
+    </message>
+    <message>
+        <source>Error reading file
+%1</source>
+        <translation>Läsfel för filen
+%1</translation>
+    </message>
+    <message>
+        <source>Empty file</source>
+        <translation>Tom fil</translation>
+    </message>
+    <message>
+        <source>--- Binary file preview clipped at %1 kB ---</source>
+        <translation>--- Binärfilsgranskning klippt vid %1 kB ---</translation>
+    </message>
+    <message>
+        <source>--- Text file preview clipped at %1 lines ---</source>
+        <translation>--- Textfilsgranskning klippt vid %1 rader ---</translation>
+    </message>
+    <message>
+        <source>--- Text file preview clipped at %1 kB ---</source>
+        <translation>--- Textfilsgranskning klippt vid %1 kB ---</translation>
+    </message>
+    <message>
+        <source>Cannot create folder %1
+Permission denied</source>
+        <translation>Kan inte skapa mappen %1
+Behörighet saknas</translation>
+    </message>
+    <message>
+        <source>Cannot create folder %1</source>
+        <translation>Kan inte skapa mappen %1</translation>
+    </message>
+    <message>
+        <source>Cannot rename %1
+%2</source>
+        <translation>Kan inte byta namn på %1
+%2</translation>
+    </message>
+    <message>
+        <source>Cannot change permissions
+%1</source>
+        <translation>Kan inte ändra behörighet
+%1</translation>
+    </message>
+</context>
+<context>
+    <name>FileWorker</name>
+    <message>
+        <source>File operation already in progress</source>
+        <translation>Filåtgärd redan påbörjad</translation>
+    </message>
+    <message>
+        <source>Empty filename</source>
+        <translation>Tomt filnamn</translation>
+    </message>
+    <message>
+        <source>File not found</source>
+        <translation>Filen kunde inte hittas</translation>
+    </message>
+    <message>
+        <source>Folder delete failed</source>
+        <translation>Mappborttagning misslyckades</translation>
+    </message>
+    <message>
+        <source>Cancelled</source>
+        <translation>Avbruten</translation>
+    </message>
+    <message>
+        <source>Source folder doesn&apos;t exist</source>
+        <translation>Källmappen finns inte</translation>
+    </message>
+    <message>
+        <source>Can&apos;t create target folder %1</source>
+        <translation>Kan inte skapa målmappen %1</translation>
+    </message>
+</context>
+<context>
+    <name>GlobalPushUpMenu</name>
+    <message>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+</context>
+<context>
+    <name>InfoPage</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+    <message>
+        <source>Copyright © %2 Jens Klingen</source>
+        <translation>Copyright © %2 Jens Klingen</translation>
+    </message>
+    <message>
+        <source>No matter how much storage
+you’ve got — it will be full.</source>
+        <translation>Hur mycket lagringsutrymme du 
+än har — det blir fullt.</translation>
+    </message>
+    <message>
+        <source>Space Inspector helps to find large folders and files on your storage.</source>
+        <translation>Space Inspector hittar stora mappar.och filer i ditt lagringsutrymme.</translation>
+    </message>
+    <message>
+        <source>Questions, problems, suggestions?</source>
+        <translation>Frågor, problem, förslag?</translation>
+    </message>
+    <message>
+        <source>Free &amp; open source!</source>
+        <translation>Fri &amp; öppen källkod!</translation>
+    </message>
+    <message>
+        <source>GPL version 3</source>
+        <translation>GPL version 3</translation>
+    </message>
+    <message>
+        <source>%1 charting implementation by Imran Ghory. Also, thanks to Kari for the excellent work on %2.</source>
+        <translation>%1 kartläggningsimplementering av Imran Ghory. Tack också till Kari för det utmärkta arbetet med %2.</translation>
+    </message>
+</context>
+<context>
+    <name>ListPage</name>
+    <message>
+        <source>Box view</source>
+        <translation>Rutvy</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>An error occurred</source>
+        <translation>Ett fel uppstod</translation>
+    </message>
+</context>
+<context>
+    <name>NodeContextMenu</name>
+    <message>
+        <source>Collapse %1</source>
+        <translation>Fäll ihop %1</translation>
+    </message>
+    <message>
+        <source>Open %1</source>
+        <translation>Öppna %1</translation>
+    </message>
+    <message>
+        <source>Delete %1</source>
+        <translation>Ta bort %1</translation>
+    </message>
+</context>
+<context>
+    <name>TreeMapNodeCollapsed</name>
+    <message numerus="yes">
+        <source>%n collapsed item(s) (%1)</source>
+        <translation>
+            <numerusform>%n dolt objekt (%1)</numerusform>
+            <numerusform>%n dolda objekt (%1)</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>TreeMapPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>List view</source>
+        <translation>Listvy</translation>
+    </message>
+    <message>
+        <source>An error occurred</source>
+        <translation>Ett fel uppstod</translation>
+    </message>
+</context>
+</TS>
